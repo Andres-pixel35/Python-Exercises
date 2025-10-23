97e36a643dfb2c687f58c33629557ece9a2c8f51
Andres Rios <riosandres294@gmail.com>
2025-10-23 15:18:15 -0500

    first steps

--- DebtTracker/main.py
+++ DebtTracker/main.py
@@ -1,1 +1,13 @@
+import helpers
+
+FILENAME = 'debt_records.csv'
+BASE_PATH = '.'
+
+old_user = helpers.file_exists(FILENAME, BASE_PATH)
+
+print("=== Welcome to your personal debt tracker! ===\n\nHere, you can not only record your debts but also automatically view their interest rates," 
+    " see how much you could save by paying them off now, and how much youcould save by increasing your monthly payments.\nMoreover," 
+    " your information will be automatically updated each time you run this programme.")
+
+   
 
